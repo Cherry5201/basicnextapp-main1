cd3c835d49ad5a0a02b42520f73fdb72e2162743
Cherry L. Tiu <ctiu@addu.edu.ph>
2026-04-08 01:58:05 +0800

    buttons

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default async function Page() {
     const session = await auth.api.getSession({
@@ -15,6 +16,29 @@ export default async function Page() {
       <h1 className="text-3xl font-bold text-gray-800 dark:text-white">
         Welcome Home
       </h1>
+
+      {/* BUTTONS SECTION */}
+      <div className="flex gap-4">
+        
+        <Link href="/uom">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+            UOM
+          </button>
+        </Link>
+
+        <Link href="/testcategories">
+          <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
+            Categories
+          </button>
+        </Link>
+
+        <Link href="/medicaltests">
+          <button className="px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-600">
+            Medical Tests
+          </button>
+        </Link>
+
+      </div>
     </div>
   );
 }
